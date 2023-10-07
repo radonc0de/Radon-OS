4bedbe8e90df9e25c759d3c15a2dfc9ccead05b1
Keith L <keithlehman@proton.me>
2023-10-07 00:27:15 -0400

    Entering protected mode

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 # Radon-OS
 Using [this Udemy course](https://www.udemy.com/course/developing-a-multithreaded-kernel-from-scratch/) as a template to build a fully-functional multithreaded kernel
 
+## Real Mode Development
+
+- For creating the bootloader, a 16-bit assembly code file is used to give the processor some very basic instructions in Real Mode. These are used to print a message, handle and invoke interrupts and access the disk
+- Limited to 1MB of memory
+
+## Protected Mode Development
+
+- Processor has different levels of protection that control how memory and hardware can be accessed by the processor
+    - Kernel runs in most priviledged ring, while programs run in least priviledged. Drivers can be ran in levels in between.
+- For 32-bit instructions, up to 4GB of memory are addressable
+- [Switching to protected mode](https://wiki.osdev.org/Protected_Mode)
+    1. `lgtdt` will load Global Descriptor Table
+    2. Set protection enable bit in CR0 (control register 0)
+    3. Jump to selector and offset of where to load
+
+
 
--- boot.asm
+++ boot.asm
@@ -1,5 +1,8 @@
-ORG 0
+ORG 0x7c00
 BITS 16
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
 
 _start:
     jmp short start
@@ -7,19 +10,25 @@ _start:
     times 33 db 0
 
 start:
-    jmp 0x7c0:step2
+    jmp 0:step2
 
 step2:
     cli ; Clear Interrupts
-    mov ax, 0x7c0
+    mov ax, 0x00
     mov ds, ax
     mov es, ax
-    mov ax, 0x00
     mov ss, ax
     mov sp, 0x7c00
     sti ; Enables Interrupts
 
-    jmp $
+.load_protected:
+    cli
+    lgdt[gdt_descriptor]
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax
+    jmp CODE_SEG:load32
+    
 
 ; GDT
 gdt_start:
@@ -33,24 +42,36 @@ gdt_code:       ; CS SHOULD POINT TO THIS
     dw 0xffff   ; Segment limit first 0-15 bits
     dw 0        ; Base first 0-15 bits
     dw 0        ; Base 16-23 bits
+    db 0x9a     ; Access byte
+    db 11001111b    ; High 4 bit flags and low 4 bit flags
+    db 0        ; Base 24-31 bits
 
+; offset 0x10
+gdt_data:       ; Linked to DS, SS, ES, FS, GS
+    dw 0xffff   ; Segment limit first 0-15 bits
+    dw 0        ; Base first 0-15 bits
+    dw 0        ; Base 16-23 bits
+    db 0x92     ; Access byte
+    db 11001111b    ; High 4 bit flags and low 4 bit flags
+    db 0        ; Base 24-31 bits
 
+gdt_end:
 
-print: 
-    mov bx, 0
-.loop:
-    lodsb
-    cmp al, 0
-    je .done
-    call print_char
-    jmp .loop
-.done:
-    ret
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
 
-print_char:
-    mov ah, 0eh
-    int 0x10
-    ret
+[BITS 32]
+load32:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov ebp, 0x00200000
+    mov esp, ebp
+    jmp $
 
 times 510-($ - $$) db 0
 dw 0xAA55
